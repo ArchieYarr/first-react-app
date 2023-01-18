b14c40b0402d439edc541b9bbbe09fb249906bda
ArchieYarr <AYARR200@gcu.ac.uk>
2023-01-18 23:44:09 +0000

    Cards still need padding and text alignment

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,10 +9,10 @@ function Home() {
         <img className=" " src={homeHeader}></img>
       </div>
 
-      <div className ="mt-8 grid grid-cols-1 px-60" >
+      <div className ="mt-8 grid grid-cols-1 px-80 gap-10" >
 
       <div className = "card lg:card-side bg-base-100 shadow-xl">
-        <img src={homeHeader} alt="filler" class="w-full h-32 sm:h-48 object-cover " ></img>
+        <figure><img src={homeHeader} alt="filler"  ></img></figure>
       <div className="card-body">
         <span className="font-bold">BBC Politics</span>
         <span className="block text-gray-500 text-sm">Designing a new BBC digital product for 18-24 year olds!</span>
@@ -29,7 +29,7 @@ function Home() {
       </div>
 
       <div className = "card lg:card-side bg-base-100 shadow-xl">
-        <img src={homeHeader} alt="filler" class="w-full h-32 sm:h-48 object-cover " ></img>
+        <figure><img src={homeHeader} alt="filler"  ></img></figure>
       <div className="card-body">
         <span className="font-bold">Portfolio Project</span>
         <span className="block text-gray-500 text-sm">Developing and designing this site!</span>
