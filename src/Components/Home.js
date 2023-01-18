70da3ae0b53d40de093a62ec4543122182582ae7
ArchieYarr <AYARR200@gcu.ac.uk>
2023-01-18 23:23:55 +0000

    Worked Daisy Cards in with Tailwind Grids

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,25 +4,39 @@ import homeHeader from "../Images/HomeImg/HomeSampleimg1.webp";
 
 function Home() {
   return (
-    <div className="grid grid-cols-1 bg-neutral gap-10">
+    <div className="grid grid-cols-1  gap-10">
       <div className = "card">
-        <img class=" " src={homeHeader}></img>
+        <img className=" " src={homeHeader}></img>
       </div>
 
-      <div className ="grid grid-cols-2 bg-neutral gap-10" >
+      <div className ="mt-8 grid grid-cols-1 px-60" >
 
-      <div className = "card">
-        <img class="w-full h-32 sm:h-48 object-cover" src={homeHeader}></img>
-      </div>
-      <div class="m-4">
-        <span class="block text-gray-500 text-sm">BBC Case Study</span>
+      <div className = "card lg:card-side bg-base-100 shadow-xl">
+        <img src={homeHeader} alt="filler" class="w-full h-32 sm:h-48 object-cover " ></img>
+      <div className="card-body">
+        <span className="font-bold">BBC Politics</span>
+        <span className="block text-gray-500 text-sm">BBC Case Study</span>
+      
+      <div className ="card-actions justify-end">
+              <div className="badge badge-outline">Research </div>
+              <div className= "badge badge-outline">and other things</div>
+            </div>
+
+            </div>
       </div>
 
-      <div className = "card">
-        <img class="w-full h-32 sm:h-48 object-cover" src={homeHeader}></img>
-      </div>
-      <div class="m-4">
-        <span class="block text-gray-500 text-sm">Portfolio Website</span>
+      <div className = "card lg:card-side bg-base-100 shadow-xl">
+        <img src={homeHeader} alt="filler" class="w-full h-32 sm:h-48 object-cover " ></img>
+      <div className="card-body">
+        <span className="font-bold">Portfolio Project</span>
+        <span className="block text-gray-500 text-sm">Developing and designing this site!</span>
+      
+      <div className ="card-actions justify-end">
+              <div className="badge badge-outline">Development </div>
+              <div className= "badge badge-outline">Design</div>
+            </div>
+
+            </div>
       </div>
 
     </div>
