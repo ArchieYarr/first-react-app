2f992ece774bee5a12ec0a92688078b19c5440a0
ArchieYarr <AYARR200@gcu.ac.uk>
2023-01-21 18:27:54 +0000

    About Formating

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,18 +5,19 @@ import CVSvg from "../Images/AboutImg/Archie Yarr CV For website.svg";
 import CV from "../Images/HomeImg/Archie Yarr CV.pdf";
 function About() {
   return (
-    <div className=" mt-8 grid-cols-1 grid-flow-col gap-10 px-96 bg-base-200 pb-52  " id = "about">
-    
-          <div class=" flex py-5 items-center pr-96 ">
-    
-            <span class="flex-shrink mx-4 ">About me</span>
-            <div class="flex-1 border-t border-accent w-24 "></div>
-    
+    <div className="  pl-52 pr-96 bg-base-200 pb-52 pt-28 " id = "about">
+    <div className = "grid grid-cols-2  md:grid-cols-2  grid-rows-2 gap-10">
+          
+          <div>
+            
+          <img className=" lg:max-w-screen-sm md:min-w-screen-sm rounded-lg shadow-2xl " src={CVImg}></img>
           </div>
-      <img className=" max-w-xl rounded-lg shadow-2xl " src={CVImg}></img>
-      <h1 className="text-5xl font-bold">Computing graduate from St Andrews, Scotland. Currently based in Glasgow.</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi. Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <a href = {CVSvg} target ="blank" className="btn btn-primary">Check out my CV</a>
+          <div>
+          <h1 className="text-5xl font-bold">Computing graduate from St Andrews, Scotland. Currently based in Glasgow.</h1>
+          <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi. Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <a href = {CV} target ="blank" className="btn btn-primary">Check out my CV</a>
+        </div>
+      </div>
     </div>
     
   
