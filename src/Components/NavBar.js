ab017436475f3c41a83c897475f25f3788f3dacd
ArchieYarr <AYARR200@gcu.ac.uk>
2023-01-23 13:11:58 +0000

    Navbar flexibility

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,34 +1,49 @@
 import React from "react";
 import { HashLink } from "react-router-hash-link";
-import {Link} from "react-scroll"
+import BurgerMenu from "../../Images/NavImg/Icons8-menu.gif";
 
 
 function NavBar() {
 
-  
+ 
     
   
   return (
-    <nav className=" bg-base-100 sticky top-0 z-50 bg-opacity-0"  >
-      <div className = "max-w-51 mx-auto px-4">
+    
+    <nav className=" bg-base-100 md:bg-base-200 sm:bg-base-200 sticky top-0 z-50 lg:bg-opacity-0 "  >
+      {/* Navbar for large screens */}
+      <div className = "g:flex max-w-51 mx-auto px-4">
       <div className = "flex items-center justify-between h-16">
-        <span className="btn btn-ghost normal-case text-xl ">Archie Yarr</span>
+        <span className="hidden lg:flex btn btn-ghost normal-case text-xl ">Archie Yarr</span>
       
       
-        <div className="flex space-x-4 px-1 ">
-          
-            
+        <div className=" hidden lg:flex  space-x-4 px-1 ">
             <HashLink className = "btn btn-ghost active:bg-primary normal-case" to = "/#home" spy={true} smooth={true} offset={50} duration={500}>Home</HashLink>
-            
             <HashLink className = "btn btn-ghost active:bg-primary normal-case" to= "/#about" spy={true} smooth={true} offset={50} duration={500} >About</HashLink>
-
-              <HashLink className = "btn btn-ghost active:bg-primary normal-case" to = '/#contact' spy={true} smooth={true} offset={50} duration={500} >Contact</HashLink>
-            
-            
-          
+              <HashLink className = "btn btn-ghost active:bg-primary normal-case" to = '/#contact' spy={true} smooth={true} offset={50} duration={500} >Contact</HashLink>  
         </div>
         </div>
       </div>
+{/* Flexible burger menu button */}
+      <div className = "lg:hidden flex items-center">
+        <button className = "outline-none mobile-menu-button"> 
+          <svg className = "w-6 h-6 text-accent"
+                x-show = "!showMenu"
+                fill = "none"
+                strokeLinecap = "round"
+                strokeLinejoin ="round"
+                strokeWidth = "2"
+                viewBox = "0 0 24 24"
+                stroke= "currentColor">
+          <path d= "M4 6h16M4 12h16M4 18h16"></path>
+          </svg>
+        </button>
+      </div>
+      <div className=" hidden mobile-menu ">
+            <HashLink className = "btn btn-ghost active:bg-primary normal-case" to = "/#home" spy={true} smooth={true} offset={50} duration={500}>Home</HashLink>
+            <HashLink className = "btn btn-ghost active:bg-primary normal-case" to= "/#about" spy={true} smooth={true} offset={50} duration={500} >About</HashLink>
+              <HashLink className = "btn btn-ghost active:bg-primary normal-case" to = '/#contact' spy={true} smooth={true} offset={50} duration={500} >Contact</HashLink>  
+        </div>
     </nav>
   );
 }
