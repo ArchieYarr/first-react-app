ed94052ef1c0564c957e09bc98a738ff426bdd11
ArchieYarr <AYARR200@gcu.ac.uk>
2023-01-26 15:07:51 +0000

    Image Auto-Slider implemented

--- src/Components/CaseStudies/BBCPoliticsComponents/OnboardingSlider.js
+++ src/Components/CaseStudies/BBCPoliticsComponents/OnboardingSlider.js
@@ -1,30 +1,68 @@
-import {React, useState} from "react"
-import Background1 from "../../../Images/BBCImg/BBCPoliticsLogo.PNG";
-import Background2 from "../../../Images/BBCImg/Onboarding 1.PNG";
-import Background3 from "../../../Images/BBCImg/Onboarding 2.PNG";
-import Background4 from "../../../Images/BBCImg/Onboarding 3.PNG";
+import {React, useState, useEffect} from "react"
 import {BsChevronCompactLeft, BsChevronCompactRight} from "react-icons/bs"
+import bBC from "../../../Images/BBCImg/BBCPoliticsLogo.PNG";
+import onboarding1 from "../../../Images/BBCImg/Onboarding1.PNG"
+import onboarding2 from "../../../Images/BBCImg/Onboarding2.PNG"
+import onboarding3 from "../../../Images/BBCImg/Onboarding3.PNG"
 
 
 function OnboardingSlider(){
-    
+    //putting imports into array 
     const slides = [
         {
-            url: "../../../Images/BBCImg/BBCPoliticsLogo.PNG"
+            url: bBC
         },
         {
-            url:"../../../Images/BBCImg/Onboarding 1.PNG"
+            url:onboarding1
         },
         {
-            url:"../../../Images/BBCImg/Onboarding 2.PNG"
+            url:onboarding2
         },
         {
-            url:"../../../Images/BBCImg/Onboarding 3.PNG"
+            url:onboarding3
         },
 
-    ]
+    ];
 
-    const [currentIndex, setCurrentIndex] = useState(0)
+    //allow the slides to be tracked for use in switching between slides in return()
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const autoScroll = true;
+  let slideInterval;
+  let intervalTime = 5000;
+
+  // Functions for moving between slides or indexes 
+    function prevSlide(){
+        const isFirstSlide = currentIndex === 0;
+        const newIndex = isFirstSlide ? slides.length -1 : currentIndex -1;
+        setCurrentIndex(newIndex);
+
+
+    }
+
+    function nextSlide(){
+        const isLastSlide = currentIndex === slides.length -1;
+        const newIndex = isLastSlide ?0 : currentIndex +1;
+        setCurrentIndex(newIndex);
+    }
+
+
+    //functions for autoscroll functionality
+    function auto() {
+        slideInterval = setInterval(nextSlide, intervalTime);
+      }
+      useEffect(() => {
+        setCurrentIndex(0);
+      }, []);
+    
+      useEffect(() => {
+        if (autoScroll) {
+          auto();
+        }
+        return () => clearInterval(slideInterval);
+      }, [currentIndex]);
+    
+
 
     return(
 
@@ -33,12 +71,12 @@ function OnboardingSlider(){
             <div style = {{backgroundImage: `url(${slides[currentIndex].url})`}} className = "w-full h-full rounded-2xl bg-center bg-cover duration-500"></div>
             {/* left Arrow */}
             <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 -translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-                <BsChevronCompactLeft size={30} />
+                <BsChevronCompactLeft onClick = {prevSlide} size={30} />
             </div>
 
             {/* Right Arrow */}
             <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 -translate-y-[50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-            <BsChevronCompactRight size={30}/>
+            <BsChevronCompactRight onClick = {nextSlide} size={30}/>
             </div>
 
         </div>
