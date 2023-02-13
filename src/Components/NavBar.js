7f4ae0a6ba502dcfd27ba5f3a75f344b60fcf85f
ArchieYarr <AYARR200@gcu.ac.uk>
2023-02-13 17:16:51 +0000

    flexible scroll navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,17 +1,36 @@
 
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { HashLink } from "react-router-hash-link";
 import BurgerMenu from "../Images/NavImg/icons8-menu.svg";
 
 
 function NavBar() {
 
-  const [isNavOpen, setIsNavOpen] = useState(false);
-    
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true)
   
+  const handleScroll = () => {
+      const currentScrollPos = window.scrollY
+  
+      if(currentScrollPos > prevScrollPos){
+          setVisible(false)
+      }else{
+          setVisible(true)
+      }
+  
+      setPrevScrollPos(currentScrollPos)
+  }
+  
+  useEffect( () => {
+      window.addEventListener('scroll', handleScroll);
+  
+      return () => window.removeEventListener('scroll', handleScroll)
+  })
+    
+  console.log(visible)
   return (
     
-    <nav className=" lg:bg-base-100  sticky top-0 z-50 lg:bg-opacity-0 "  >
+    <nav className={' lg:bg-base-100  sticky top-0 z-50 lg:bg-opacity-0'} >
       {/* Navbar for large screens */}
       
       <div className = " flex items-center justify-between h-16">
@@ -20,7 +39,7 @@ function NavBar() {
       
         <div className="  lg:flex  space-x-4 px-1 ">
             <HashLink className = "btn btn-ghost active:bg-primary normal-case" to = "/#home" spy={true.toString()} smooth={true} offset={50} duration={500}>Home</HashLink>
-            {/* <HashLink className = "btn btn-ghost active:bg-primary normal-case" to = "/#Study1" spy={true} smooth={true} offset={50} duration={500}>Work</HashLink> */}
+            
             <HashLink className = "btn btn-ghost active:bg-primary normal-case" to= "/#about" spy={true.toString()} smooth={true} offset={50} duration={500} >About</HashLink>
             
               <HashLink className = "btn btn-ghost active:bg-primary normal-case" to = '#contact' spy={true.toString()} smooth={true} offset={50} duration={500} >Contact</HashLink>  
@@ -29,7 +48,7 @@ function NavBar() {
       
 {/* Flexible burger menu button to go here*/}
     
-        
+{/* lg:bg-base-100  sticky top-0 z-50 lg:bg-opacity-0 */}
     </nav>
   );
 }
