2f89de1f2ec7154cd661e780de47f430ee28f8cf
ArchieYarr <AYARR200@gcu.ac.uk>
2023-02-14 12:37:59 +0000

    responsive navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -7,8 +7,8 @@ import BurgerMenu from "../Images/NavImg/burger menu.svg";
 
 function NavBar() {
 
-let [open, setOpen] = useState(false)
-  
+
+  let [open, setOpen]= useState(false)
   return (
     
     <nav className={'bg-base-200 md:bg-base-100  sticky top-0 z-50 md:bg-opacity-0'} >
@@ -19,12 +19,13 @@ let [open, setOpen] = useState(false)
         <HashLink className=" lg:flex btn btn-ghost normal-case text-xl " to = "/#home" spy={true.toString()} smooth={true} offset={50} duration={500}>Archie Yarr</HashLink>
         </div>
 
-        <div onClick = {()=>setOpen(!open)} className=" text-3xl absolute right-8 cursor-pointer md:hidden">
-        <ion-icon name="{open ? 'close':'menu'}"></ion-icon>
-         
+        <div onClick = {()=>setOpen(!open)} className=" text-3xl absolute right-8 top-0 cursor-pointer md:hidden">
+        
+        <button className={open ? "hidden":"" }><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg></button>
+        <button className={open ? "": "hidden"}><svg xmlns="http://www.w3.org/2000/svg" className="inline-block h-5 w-5 stroke-current" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg></button>
         </div>
 
-        <ul className={"bg-base-200 md:bg-base-100 md:flex md:items-center md:pb-0 pb-4 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 transition-all duration-500 ease-in"}>
+        <ul className={"bg-base-200 md:bg-base-100 md:flex md:items-center md:pb-0 pb-4 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 transition-all duration-500 ease-in   "}>
            <li> 
             <HashLink className = "btn btn-ghost active:bg-primary normal-case" to = "/#home" spy={true.toString()} smooth={true} offset={50} duration={500}>Home</HashLink>
            </li>
