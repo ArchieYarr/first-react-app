00861c829680390273e69ba7a0d3dc1cbb69d89d
ArchieYarr <AYARR200@gcu.ac.uk>
2023-02-14 11:56:40 +0000

    responsiveness

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,7 +2,7 @@
 import { React, useState, useEffect } from "react";
 import { HashLink } from "react-router-hash-link";
 import BurgerMenu from "../Images/NavImg/burger menu.svg";
-import {AiFillDelete, AiOutlineMenuUnfold} from "react-icons/ai"
+
 
 
 function NavBar() {
@@ -19,9 +19,9 @@ let [open, setOpen] = useState(false)
         <HashLink className=" lg:flex btn btn-ghost normal-case text-xl " to = "/#home" spy={true.toString()} smooth={true} offset={50} duration={500}>Archie Yarr</HashLink>
         </div>
 
-        <div onClick = {()=>setOpen(!open)} className=" text-3xl absolute right-8 top-0 cursor-pointer md:hidden">
-        <ion-icon name= {open ? 'close': 'menu'}></ion-icon>
-          {/* <button name = {open ? 'close': 'menu' }  className="btn btn-square btn-ghost px-0"  > <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg></button> */}
+        <div onClick = {()=>setOpen(!open)} className=" text-3xl absolute right-8 cursor-pointer md:hidden">
+        <ion-icon name="{open ? 'close':'menu'}"></ion-icon>
+         
         </div>
 
         <ul className={"bg-base-200 md:bg-base-100 md:flex md:items-center md:pb-0 pb-4 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 transition-all duration-500 ease-in"}>
