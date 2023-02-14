81e513d9fef3c9d177cbf777c90f77231d235828
ArchieYarr <AYARR200@gcu.ac.uk>
2023-02-14 10:37:51 +0000

    responsive navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,32 +2,13 @@
 import { React, useState, useEffect } from "react";
 import { HashLink } from "react-router-hash-link";
 import BurgerMenu from "../Images/NavImg/icons8-menu.svg";
+import {AiFillDelete, AiOutlineMenuUnfold} from "react-icons/ai"
 
 
 function NavBar() {
 
-  const [prevScrollPos, setPrevScrollPos] = useState(0);
-  const [visible, setVisible] = useState(true)
+  const [open, setOpen] = useState(false)
   
-  const handleScroll = () => {
-      const currentScrollPos = window.scrollY
-  
-      if(currentScrollPos > prevScrollPos){
-          setVisible(false)
-      }else{
-          setVisible(true)
-      }
-  
-      setPrevScrollPos(currentScrollPos)
-  }
-  
-  useEffect( () => {
-      window.addEventListener('scroll', handleScroll);
-  
-      return () => window.removeEventListener('scroll', handleScroll)
-  })
-    
-  console.log(visible)
   return (
     
     <nav className={' lg:bg-base-100  sticky top-0 z-50 lg:bg-opacity-0'} >
@@ -36,8 +17,16 @@ function NavBar() {
       <div className = " flex items-center justify-between h-16">
         <HashLink className=" lg:flex btn btn-ghost normal-case text-xl " to = "/#home" spy={true.toString()} smooth={true} offset={50} duration={500}>Archie Yarr</HashLink>
       
-      
-        <div className="  lg:flex  space-x-4 px-1 ">
+        <div onClick = {()=> setOpen(!open)} className="tesxt-3xl absolute right-8 top-5 cursor-pointer lg:hidden">
+
+{
+
+  open ? <AiFillDelete />: <AiOutlineMenuUnfold/>
+
+}
+
+</div>
+        <div className={"  lg:flex  space-x-4 px-1 ${open ? 'top-18 opacity-100' : 'top-[-490] lg:opacity-100 opacity-0' } "}>
             <HashLink className = "btn btn-ghost active:bg-primary normal-case" to = "/#home" spy={true.toString()} smooth={true} offset={50} duration={500}>Home</HashLink>
             
             <HashLink className = "btn btn-ghost active:bg-primary normal-case" to= "/#about" spy={true.toString()} smooth={true} offset={50} duration={500} >About</HashLink>
@@ -48,7 +37,7 @@ function NavBar() {
       
 {/* Flexible burger menu button to go here*/}
     
-{/* lg:bg-base-100  sticky top-0 z-50 lg:bg-opacity-0 */}
+
     </nav>
   );
 }
