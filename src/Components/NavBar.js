acca3f0bee9a7a6f26175785627b226e38744ee8
ArchieYarr <AYARR200@gcu.ac.uk>
2023-02-15 15:54:32 +0000

    Error corrected ref moved to general div

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -27,7 +27,7 @@ function NavBar() {
     <nav className={' bg-base-100  sticky top-0 z-50 lg:bg-opacity-0'} >
       {/* Navbar for large screens */}
       
-      <div  className = " lg:flex lg:items-center justify-between h-13 left-0  " >
+      <div ref = {menuRef} className = " lg:flex lg:items-center justify-between h-13 left-0  " >
         <div>
         <HashLink onClick = {()=>setOpen(!open)} className={ open ? "motion-safe:animate-fadeIn lg:flex btn btn-ghost normal-case text-2xl md:text-xl ": " lg:flex btn btn-ghost normal-case text-2xl md:text-xl"} to = "/#home" spy={true.toString()} smooth={true} offset={50} duration={500}>Archie Yarr</HashLink>
         </div>
@@ -38,14 +38,14 @@ function NavBar() {
         <button className={open ? "btn btn-ghost normal-case text-xl": "hidden btn btn-ghost normal-case text-xl"}><svg xmlns="http://www.w3.org/2000/svg" className="inline-block h-7 w-7 stroke-current" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg></button>
         </div>
 
-        <ul ref = {menuRef} className={`bg-base-100 lg:bg-opacity-0 lg:flex lg:items-center lg:pb-0 absolute lg:static lg:z-auto z-[-1] left-0  w-full lg:w-auto lg:pl-0 transition-all duration-500 ease-in ${open ? 'top-12 ':'top-[-490px]'}`}>
+        <ul  className={`bg-base-100 lg:bg-opacity-0 lg:flex lg:items-center lg:pb-0 absolute lg:static lg:z-auto z-[-1] left-0  w-full lg:w-auto lg:pl-0 transition-all duration-500 ease-in ${open ? 'top-12 ':'top-[-490px]'}`}>
            <li> 
             <HashLink className = "btn btn-ghost active:bg-primary normal-case" to = "/#home" spy={true.toString()} smooth={true} offset={50} duration={500}>Home</HashLink>
            </li>
            <li>
             <HashLink className = "btn btn-ghost active:bg-primary normal-case" to= "/#about" spy={true.toString()} smooth={true} offset={50} duration={500} >About</HashLink>
             </li>
-            <li>
+            <li >
               <HashLink className = "btn btn-ghost active:bg-primary normal-case" to = '#contact' spy={true.toString()} smooth={true} offset={50} duration={500} >Contact</HashLink>  
               </li>
         </ul>
