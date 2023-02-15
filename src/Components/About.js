36c477c8c3ebccc279ebd6db0059943ea7b1463e
ArchieYarr <AYARR200@gcu.ac.uk>
2023-02-15 14:28:10 +0000

    About content

--- src/Components/About.js
+++ src/Components/About.js
@@ -14,15 +14,21 @@ function About() {
     
             <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold  ">GCU Computing graduate from St Andrews, Scotland. Currently based in Glasgow.</h1>
             <div className="text-justify">
-              <p className="pt-8 pb-2">I'm a curios designer and technology geek who's committed to advocating for the user wherever I'm working. I believe that effective user centric design can have real impact on peoples lives no matter how big or small. </p>
+              <p className="pt-8 pb-2">
+                I'm a curios designer and technology geek who's committed to advocating for the user wherever I'm working. 
+                I believe that effective user centric design can have real impact on peoples lives no matter how big or small. 
+              </p>
+
               <p className="py-2 ">
               As a designer, I love tackling knotty challenges. From prototyping and wireframing to JavaScript and CSS, contributing to a team and learning from others is my favourite part of the process.  I've experienced the design 
               process academically and vocationally and nothing else has got me as excited as seeing a component, interaction or process being shaped by the people working on it.
-              As a person, I enjoy cooking, training, technology and walking with friends. And when I have the chance reading and sketching are my go-to ways to relax. </p>
+              As a person, I enjoy cooking, training, technology and walking with friends. And when I have the chance reading and sketching are my go-to ways to relax. 
+              </p>
+              
               <p className="py-2">
                 A goal of mine is to combine what I do for fun with my career opportunities. So joining a field that works creatively, collaboratively and with technology 
                 aligns with my goals and is an area that I can't wait to contribute to.
-                </p>
+              </p>
             </div>
             <a href = {CV} rel="noreferrer" target ="blank" className="btn btn-primary">Check out my CV</a>
           </div>
