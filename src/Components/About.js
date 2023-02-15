b6fd23b72e4a4799e8c521d66e58f39352e85006
ArchieYarr <AYARR200@gcu.ac.uk>
2023-02-15 14:35:17 +0000

    About content and padding

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,7 +5,7 @@ import CV from "../Images/HomeImg/Archie Yarr CV.pdf";
 
 function About() {
   return (
-    <div className="flex flex-col md:flex-row min-w-screen    md:px-8   py-8   md:pt-4     bg-base-200 shadow-xl" >
+    <div className="flex flex-col md:flex-row min-w-screen  lg:px-32  md:px-8   py-12   md:pt-4     bg-base-200 shadow-xl" >
       <div className="bg-base-200 h-screen-md  md:max-h-screen   ">
     <HashLink className = "" to= "/#about" spy={true.toString()} smooth={true} offset={50} duration={500}><img className="hidden md:block lg:hover:scale-110 md:hover:scale-105     px-1 md:px-0 top-0  lg:-mt-52 md:-mt-24 -mt-12    lg:max-w-screen-sm lg:max-h-screen-sm  max-h-sm max-w-sm  rounded-lg h-initial  " src={CVImg}></img></HashLink>
     </div>
@@ -13,7 +13,7 @@ function About() {
     <div className=" flex flex-col  place-items-start px-8 ">
     
             <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold  ">GCU Computing graduate from St Andrews, Scotland. Currently based in Glasgow.</h1>
-            <div className="text-justify">
+            <div className="text-left">
               <p className="pt-8 pb-2">
                 I'm a curios designer and technology geek who's committed to advocating for the user wherever I'm working. 
                 I believe that effective user centric design can have real impact on peoples lives no matter how big or small. 
@@ -24,7 +24,7 @@ function About() {
               process academically and vocationally and nothing else has got me as excited as seeing a component, interaction or process being shaped by the people working on it.
               As a person, I enjoy cooking, training, technology and walking with friends. And when I have the chance reading and sketching are my go-to ways to relax. 
               </p>
-              
+
               <p className="py-2">
                 A goal of mine is to combine what I do for fun with my career opportunities. So joining a field that works creatively, collaboratively and with technology 
                 aligns with my goals and is an area that I can't wait to contribute to.
